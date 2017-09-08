7192619c1e473f8f1eb9bfb105ff084b71a25ee5
Chris <christopher.petrucelli1@marist.edu>
2017-09-08 11:53:35 -0400

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,5 @@
+noun = input("Enter a noun ")
+verb = input("Enter a verb in the past tense ")
+adj = input("Enter an adjective ")
+place = input("Enter a body part ")
+print("Earlier today, I was at the store when a", adj, noun, verb, "into my", place, ".")
