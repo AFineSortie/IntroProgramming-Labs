c5b46b948b66b70f2691971959ce536efdf75c8b
Chris <christopher.petrucelli1@marist.edu>
2017-09-22 11:45:25 -0400

    Usability improvements to guessing game. Resolves #5 and resolves #6.

--- guessing-game.py
+++ guessing-game.py
@@ -1,13 +1,30 @@
 mysteryAnimal = "snake"
+quitVar = "q"
 
 while True:
     print("\nI'm thinking of an animal...")
     guess = input("\nTake a guess what it is: ").lower()
+
     if guess == mysteryAnimal:
+        print("\nThat's the one! Congratulations!")
+
+        snakeLike = input("\nDo you like snakes y/n: ").lower()
+        if snakeLike[0] == "y":
+            print("\nAwww yea")
+
+        elif snakeLike[0] == "n":
+            print("\nGet out")
+
+        else:
+            print("\nThats not an answer I'm looking for!")
+                
         break
-    elif guess == "quit":
+
+    elif guess[0] == quitVar:
+        print("You quitter!")
         break
+
     else:
         print("\nThat's not it... maybe you should try again?")
 
-print("\nThat's the one! Congratulations!")
+
