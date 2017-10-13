556b23badbd77499740e540a334e2a459a88c7a3
Chris <christopher.petrucelli1@marist.edu>
2017-10-13 11:45:01 -0400

    If the command is invalid then the quit function runs.

---
+++ arithmetic-engine.py
@@ -0,0 +1,45 @@
+#Arithmetic Engine
+
+#Show Intro
+def intro():
+    print("This is the Arithmetic Engine. This program accepts a user command (add, mult, diff, quot, quit) and two numbers and calculates the correct command with those given numbers")
+
+#Loop
+def engine():
+    while True:
+        cmd = input("Enter the desired command: ").lower()
+        if cmd == "quit":
+            break
+
+        num1 = int(input("Enter the first number: "))
+        num2 = int(input("Enter the second number: "))
+
+        answer = 0
+        if cmd == "add":
+            answer = num1 + num2
+            print(answer)
+        elif cmd == "mult":
+            answer = num1 * num2
+            print(answer)
+        elif cmd == "diff":
+            answer = num1 - num2
+            print(answer)
+        elif cmd == "quot":
+            answer = num1 / num2
+            print(answer)
+        else:
+            print("That was not a valid command!")
+            quit()
+
+def end():
+    print("Thanks for stoppin' by!")
+
+
+
+def main():
+    intro()
+    engine()
+    end()
+
+
+main()
